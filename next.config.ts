66984965175c6c73d67a07da7e6b4d5c78f2eb4a
gg8899 <528032175@qq.com>
2026-05-03 17:50:10 +0800

    add workflow

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  output: 'export', // 关键：开启静态导出
   images: {
-    unoptimized: true
+    unoptimized: true, // GitHub Pages 不支持 next/image 优化
   },
+  basePath: '/shenghuida', // ⚠️ 必填（repo 名）
+  assetPrefix: '/shenghuida/',
 };
 
 export default nextConfig;
